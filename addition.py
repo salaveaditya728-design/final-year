4f60e59553538d7ff10930057db5b323a4220b12
salaveaditya728-design <salaveaditya728@gmail.com>
2026-04-22 07:18:20 +0000

    Add addition.py program

--- addition.py
+++ addition.py
@@ -1,4 +1,6 @@
 # write the python programm for the addition of two number
 a=float(input("enter the first nnumber:"))
 b=float(input("enter the second number: "))
-print("addition({},{})={}".format(a,b,a+b))
+print("addition({},{})={}".format(a,b,a+b))git add addition.py
+git commit -m "Add addition.py program"
+git push origin main
