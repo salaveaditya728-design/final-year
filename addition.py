0ca1202f194a98ad621cfb924f9b1a02289c5145
salaveaditya728-design <salaveaditya728@gmail.com>
2026-04-22 07:17:47 +0000

    Add addition.py program

---
+++ addition.py
@@ -0,0 +1,4 @@
+# write the python programm for the addition of two number
+a=float(input("enter the first nnumber:"))
+b=float(input("enter the second number: "))
+print("addition({},{})={}".format(a,b,a+b))
