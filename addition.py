e6170518920d76a23ca1e9f7671adf2366093746
salaveaditya728-design <salaveaditya728@gmail.com>
2026-04-22 07:20:21 +0000

    Add instruction comment to addition.py

--- addition.py
+++ addition.py
@@ -1,6 +1,5 @@
+# This program adds two numbers entered by the user.
 # write the python programm for the addition of two number
 a=float(input("enter the first nnumber:"))
 b=float(input("enter the second number: "))
-print("addition({},{})={}".format(a,b,a+b))git add addition.py
-git commit -m "Add addition.py program"
-git push origin main
+print("addition({},{})={}".format(a,b,a+b))
